5bc2a9c36be23a276957568cbaea0712054dcee7
James Ioppolo <james.ioppolo@gmail.com>
2017-04-17 16:32:53 +0800

    Created a service for the configuration retreiver

--- speedtest-net-cli/Program.cs
+++ speedtest-net-cli/Program.cs
@@ -1,37 +1,13 @@
-﻿using System;
-using System.Net;
-using System.Net.Http;
-using System.Net.Http.Headers;
+﻿using SpeedtestNetCli.Services;
 
-namespace speedtest_net_cli
+namespace SpeedtestNetCli
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var httpHandler = new HttpClientHandler()
-            {
-                AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate,
-            };
-            using (var client = new HttpClient(httpHandler))
-            {
-                var request = new HttpRequestMessage()
-                {
-                    RequestUri = new Uri("http://www.speedtest.net/speedtest-config.php"),
-                    Method = HttpMethod.Get,
-                };
-                client.DefaultRequestHeaders.TryAddWithoutValidation("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8");
-                client.DefaultRequestHeaders.TryAddWithoutValidation("Accept-Encoding", "gzip, deflate, sdch");
-                client.DefaultRequestHeaders.TryAddWithoutValidation("User-Agent", "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36");
-                client.DefaultRequestHeaders.TryAddWithoutValidation("Accept-Language", "en-US,en;q=0.8");
-
-                client.DefaultRequestHeaders.CacheControl = new CacheControlHeaderValue();
-                client.DefaultRequestHeaders.CacheControl.NoCache = true;
-
-                var guid = Guid.NewGuid();
-                var response = client.GetAsync("http://www.speedtest.net/speedtest-config.php?x=" + guid.ToString()).Result;
-                var text = response.Content.ReadAsStringAsync().Result;
-            }
+            var retriever = new SpeedtestConfigurationRetriever();
+            var doc = retriever.GetConfig().Result;
         }
     }
 }
---
+++ speedtest-net-cli/Services/SpeedtestConfigurationRetriever.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace SpeedtestNetCli.Services
+{
+    public interface ISpeedtestConfigurationRetriever
+    {
+        Task<XmlDocument> GetConfig();
+    }
+
+    public class SpeedtestConfigurationRetriever : ISpeedtestConfigurationRetriever
+    {
+        XmlDocument xmlDocument = new XmlDocument();
+
+        public async Task<XmlDocument> GetConfig()
+        {
+            var httpHandler = new HttpClientHandler()
+            {
+                AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate,
+            };
+            using (var client = new HttpClient(httpHandler))
+            {
+                client.DefaultRequestHeaders.TryAddWithoutValidation("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8");
+                client.DefaultRequestHeaders.TryAddWithoutValidation("Accept-Encoding", "gzip, deflate, sdch");
+                client.DefaultRequestHeaders.TryAddWithoutValidation("User-Agent", "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36");
+                client.DefaultRequestHeaders.TryAddWithoutValidation("Accept-Language", "en-US,en;q=0.8");
+
+                client.DefaultRequestHeaders.CacheControl = new CacheControlHeaderValue();
+                client.DefaultRequestHeaders.CacheControl.NoCache = true;
+
+                var response = await client.GetAsync("http://www.speedtest.net/speedtest-config.php?x=" + Guid.NewGuid().ToString());
+                xmlDocument.LoadXml(await response.Content.ReadAsStringAsync());
+                return xmlDocument;
+            }
+        }
+    }
+}
