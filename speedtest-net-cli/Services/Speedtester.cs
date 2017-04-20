f18651621bb27c34421fcb2c7840d6beae31dd4d
James Ioppolo <james.ioppolo@gmail.com>
2017-04-20 20:53:19 +0800

    Added latency determination

--- speedtest-net-cli/Services/Speedtester.cs
+++ speedtest-net-cli/Services/Speedtester.cs
@@ -1,6 +1,9 @@
 ﻿using SpeedtestNetCli.Utilities;
 using System;
+using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Xml.Linq;
 
 namespace SpeedtestNetCli.Services
@@ -36,6 +39,38 @@ namespace SpeedtestNetCli.Services
             var closestFiveServers = servers.Descendants("server")
                                             .OrderBy(server => Convert.ToDouble(server.Attribute("clientDistance").Value))
                                             .Take(5);
+
+            for (var latencyIteration = 0; latencyIteration < 3; latencyIteration++)
+            {
+                var latencyValue = PerformLatencyTest();
+            }
+        }
+
+        private double PerformLatencyTest()
+        {
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.TryAddWithoutValidation("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8");
+                client.DefaultRequestHeaders.TryAddWithoutValidation("Accept-Encoding", "gzip, deflate, sdch");
+                client.DefaultRequestHeaders.TryAddWithoutValidation("User-Agent", "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36");
+                client.DefaultRequestHeaders.TryAddWithoutValidation("Accept-Language", "en-US,en;q=0.8");
+
+                client.DefaultRequestHeaders.CacheControl = new CacheControlHeaderValue();
+                client.DefaultRequestHeaders.CacheControl.NoCache = true;
+
+                try
+                {
+                    var sw = new Stopwatch();
+                    sw.Start();
+                    var response = client.GetAsync("http://rockingham.wa.speedtest.optusnet.com.au/speedtest/latency.txt").Result;
+                    return sw.ElapsedMilliseconds;
+                }
+                catch (HttpRequestException)
+                {
+                    return 3600;
+                }
+                
+            }
         }
     }
 }
