0b827e086e86da4761cec319df5f9d1de4ee980d
James Ioppolo <james.ioppolo@gmail.com>
2017-04-24 13:56:04 +0800

    Updated payload for uploader

--- speedtest-net-cli/Services/UploadSpeedTester.cs
+++ speedtest-net-cli/Services/UploadSpeedTester.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.Text;
 using System.Xml.Linq;
 
 namespace SpeedtestNetCli.Services
@@ -12,6 +13,16 @@ namespace SpeedtestNetCli.Services
     {
         public double GetSpeedMbps(XElement server)
         {
+            var uploadSizes = new List<int> { 32768, 65536, 131072, 262144, 524288, 1048576, 7340032 };
+            const string chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            var payload = "content1=";
+            var numIterations = 524288 / chars.Length;
+            for (var iter = 0; iter < numIterations; iter++)
+            {
+                payload += chars;
+            }
+            var bytes = Encoding.ASCII.GetBytes(payload);
+
             return 0;
         }
     }
